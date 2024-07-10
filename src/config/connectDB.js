d5d3b4a16340e33c65091cd2e2b9b1486c1f2ed7
digitalbundlekits <digitalbundlekits@gmail.com>
2024-07-10 14:02:09 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: '194.233.69.9',
-    user: 'kbmplay',
-    password: 'kbmplay',
-    database: 'kbmplay',
+    host: '148.251.91.91',
+    user: 'champ11_brt4r',
+    password: 'champ11_brt4r',
+    database: 'champ11_brt4r',
 });
 
 export default connection;
